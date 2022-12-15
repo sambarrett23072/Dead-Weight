572959b21b48af83e06759e9f6173d10ef5b79d5
Sam Barrett <sambarrett23072@gmail.com>
2022-12-15 02:18:25 +0000

    Change command prefix to $

--- responses.py
+++ responses.py
@@ -3,13 +3,13 @@ import random
 
 def handle_response(message):
     p_message = message.lower()
-    if p_message == 'hello':
+    if p_message == '$hello':
         return 'Hey there!'
 
-    if p_message == 'roll':
+    if p_message == '$roll':
         return str(random.randint(1, 6))
 
-    if p_message == '!help':
+    if p_message == '$help':
         return "`This is a help message that you can modify.`"
 
     #  return 'Yeah, I don\'t know. Try typing "!help".'
